e937ce3b0f94cc347fb752f8f01fb89208da68d1
GUILHERME <89171242+guilherme3381@users.noreply.github.com>
2022-07-29 20:20:29 -0300

    Add files via upload

---
+++ aatv,cesu/index.php
@@ -0,0 +1,60 @@
+
+
+<!DOCTYPE html>
+
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <title>marmitex</title>
+        <link rel="stylesheet" type="text/css" href="stilo.css"/>
+    </head>
+    <body>
+        <div id="barra-topo"><h1>Tele entrega, Peca JA!!</h1></div>
+        
+        <div id="menu">
+        <ul>
+            <li><a href="#">INICIO </a> </li>
+            <li><a href="#">Blog:Dirce-marmitex.com.br</a></li>
+            <li><a href="#">Conheca Dona dirce </a></li>
+            <li> <a href="#3133845">Contato:31 3845-2962</a> </li>
+            
+              
+        </ul>
+            </div>
+        
+        <div id="destaque">
+            <img src="img/1.png">
+            
+        </div>
+        <div id="centro">
+            <div id="fotos">
+                <h2>Cardapio da semana:</h2>
+                
+                <img id="foto2" src="img/2.png">
+                <img id="foto8" src="img/8.png">
+                <img id="foto4" src="img/4.png">
+                <img id="foto5" src="img/5.png">
+                <img id="foto6" src="img/6.png">
+                <img id="foto7" src="img/7.png">
+                
+                     
+            </div>
+            <div id="precos">
+                <h2>PRECOS:</h2>
+                <ul>
+                    <li>Strogonof= $20,00 </li>
+                     <li>Coma tudo,hamburgue e dog= $28,00 </li>
+                     <li>kit almoco e camarao= $60,00  </li>
+                     <li>Lasanha= $35,00 </li>
+                     <li>Feijoada moderna= $40,00 </li>
+                     <li>Costela com fritas= $75,50 </li>
+                </ul>
+            </div>
+            
+            
+        </div>
+        <div id="rodape">
+            @guilherme jonathan dos santos
+        </div>
+    </body>
+</html>
